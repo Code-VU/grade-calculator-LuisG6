c0596e922232c8ea56f5bbfe1158961bb1ad30c8
Luis Guevara <luisg@Luiss-MacBook-Pro.local>
2022-02-02 20:50:20 -0600

    adding grading code

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,8 +2,26 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
-
+    
+try:
+       
     hrs = float(input("Enter score:"))
+       
+    if hrs >= 0.9 and hrs <= 1.0:
+        print("A")
+    elif hrs >= 0.8 and hrs < 0.9:
+        print("B")
+    elif hrs >= 0.7 and hrs < 0.8:
+        print("C")
+    elif hrs >= 0.6 and hrs < 0.7:
+        print("D")
+    elif hrs < 0.6:
+        print("F")
+    else:
+        print("Bad score")
+            
+except:
+    print("Please enter a number")
 
     # end assignment
 
